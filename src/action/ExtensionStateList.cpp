63472f6b048bb24e933fe64d9fa8650e14194ea1
Chris Walker <kris240376@gmail.com>
2026-03-14 19:04:06 -0500

    add ExtensionStateList action

---
+++ src/action/ExtensionStateList.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#include "c++ami/action/ExtensionStateList.hpp"
+
+using namespace cpp_ami::action;
+
+ExtensionStateList::ExtensionStateList()
+    : Action("ExtensionStateList", { })
+{
+}
