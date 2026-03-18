d59b567cc05c4f31c7f22be1ecced84744685f39
Chris Walker <kris240376@gmail.com>
2026-03-17 19:45:51 -0500

    add Setvar action

---
+++ include/c++ami/action/Setvar.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_SETVAR_HPP
+#define ACTION_SETVAR_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class Setvar : public Action {
+public:
+    Setvar();
+};
+
+} // namespace cpp_ami::action
+
+#endif
