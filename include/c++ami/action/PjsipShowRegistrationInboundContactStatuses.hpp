d0c7ef82b9f2eef1794fab14865f9bd9ec90713c
Chris Walker <kris240376@gmail.com>
2026-03-18 19:40:24 -0500

    add PJSIPShowRegistrationInboundContactStatuses action

---
+++ include/c++ami/action/PjsipShowRegistrationInboundContactStatuses.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_REGISTRATION_INBOUND_CONTACT_STATUSES_HPP
+#define ACTION_PJSIP_SHOW_REGISTRATION_INBOUND_CONTACT_STATUSES_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowRegistrationInboundContactStatuses : public Action {
+public:
+    PJSIPShowRegistrationInboundContactStatuses();
+};
+
+} // namespace cpp_ami::action
+
+#endif
