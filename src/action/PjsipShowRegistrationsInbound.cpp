90a418da65b9741f25722b6413044c1ef07ffec8
Chris Walker <kris240376@gmail.com>
2026-03-27 23:54:07 -0500

    add PJSIPShowRegistrationsInbound action

---
+++ src/action/PjsipShowRegistrationsInbound.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#include "c++ami/action/PjsipShowRegistrationsInbound.hpp"
+
+using namespace cpp_ami::action;
+
+PJSIPShowRegistrationsInbound::PJSIPShowRegistrationsInbound()
+    : Action("PJSIPShowRegistrationsInbound")
+{
+}
