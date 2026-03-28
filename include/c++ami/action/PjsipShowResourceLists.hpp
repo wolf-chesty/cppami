f958c52750575a4d905b25b04226b369d38cb039
Chris Walker <kris240376@gmail.com>
2026-03-28 00:02:45 -0500

    add PJSIPShowResourceLists action

---
+++ include/c++ami/action/PjsipShowResourceLists.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_RESOURCE_LISTS_HPP
+#define ACTION_PJSIP_SHOW_RESOURCE_LISTS_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowResourceLists : public Action {
+public:
+    PJSIPShowResourceLists();
+};
+
+} // namespace cpp_ami::action
+
+#endif
