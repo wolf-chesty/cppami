6f8dd20874078d261e86f14c590700f388b9fcc1
Chris Walker <kris240376@gmail.com>
2026-03-27 23:39:12 -0500

    add PJSIPShowContacts action

---
+++ include/c++ami/action/PjsipShowContacts.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_CONTACTS_HPP
+#define ACTION_PJSIP_SHOW_CONTACTS_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowContacts : public Action {
+public:
+    PJSIPShowContacts();
+};
+
+} // namespace cpp_ami::action
+
+#endif
