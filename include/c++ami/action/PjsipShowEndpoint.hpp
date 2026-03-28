6e3a2e4935ab68844cede02291e84bde3ee8ee0a
Chris Walker <kris240376@gmail.com>
2026-03-27 23:46:25 -0500

    add PJSIPShowEndpoint action

---
+++ include/c++ami/action/PjsipShowEndpoint.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_ENDPOINT_HPP
+#define ACTION_PJSIP_SHOW_ENDPOINT_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowEndpoint : public Action {
+public:
+    PJSIPShowEndpoint();
+};
+
+} // namespace cpp_ami::action
+
+#endif
---
+++ src/action/PjsipShowEndpoint.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#include "c++ami/action/PjsipShowEndpoint.hpp"
+
+using namespace cpp_ami::action;
+
+PJSIPShowEndpoint::PJSIPShowEndpoint()
+    : Action("PJSIPShowEndPoint", {"Endpoint"})
+{
+}
