ac9900dcdb7bcb96d2f6982b92dc95d5cb4906f7
Chris Walker <kris240376@gmail.com>
2026-03-27 23:24:16 -0500

    add PJSIPShowAuths action

---
+++ include/c++ami/action/PjsipShowAuths.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_AUTHS_HPP
+#define ACTION_PJSIP_SHOW_AUTHS_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowAuths : public Action {
+public:
+    PJSIPShowAuths();
+};
+
+} // namespace cpp_ami::action
+
+#endif
---
+++ src/action/PjsipShowAuths.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#include "c++ami/action/PjsipShowAuths.hpp"
+
+using namespace cpp_ami::action;
+
+PJSIPShowAuths::PJSIPShowAuths()
+    : Action("PJSIPShowAuths")
+{
+}
