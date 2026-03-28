53eddef3dbedaa76b5b4c57baa8c72b42ccbb18a
Chris Walker <kris240376@gmail.com>
2026-03-27 23:58:41 -0500

    add PJSIPShowRegistrationsOutbound action

---
+++ include/c++ami/action/PjsipShowRegistrationsOutbound.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_REGISTRATIONS_OUTBOUND_HPP
+#define ACTION_PJSIP_SHOW_REGISTRATIONS_OUTBOUND_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowRegistrationsOutbound : public Action {
+public:
+    PJSIPShowRegistrationsOutbound();
+};
+
+} // namespace cpp_ami::action
+
+#endif
