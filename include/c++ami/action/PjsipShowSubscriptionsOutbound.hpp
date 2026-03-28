a5fe8fced90ec46243da2213901113ac244e2006
Chris Walker <kris240376@gmail.com>
2026-03-28 00:12:01 -0500

    add PJSIPShowSubscriptionsInbound/PJSIPShowSubscriptionsOutbound actions

---
+++ include/c++ami/action/PjsipShowSubscriptionsOutbound.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#ifndef ACTION_PJSIP_SHOW_SUBSCRIPTIONS_OUTBOUND_HPP
+#define ACTION_PJSIP_SHOW_SUBSCRIPTIONS_OUTBOUND_HPP
+
+#include "c++ami/action/Action.hpp"
+
+namespace cpp_ami::action {
+
+class PJSIPShowSubscriptionsOutbound : public Action {
+public:
+    PJSIPShowSubscriptionsOutbound();
+};
+
+} // namespace cpp_ami::action
+
+#endif
---
+++ src/action/PjsipShowSubscriptionsOutbound.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Christopher L Walker
+// SPDX-License-Identifier: MIT
+
+#include "c++ami/action/PjsipShowSubscriptionsOutbound.hpp"
+
+using namespace cpp_ami::action;
+
+PJSIPShowSubscriptionsOutbound::PJSIPShowSubscriptionsOutbound()
+    : Action("PJSIPShowSubscriptionsOutbound")
+{
+}
